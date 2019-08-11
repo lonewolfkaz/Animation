4f3bd98efa6ef8746ad52cbd2454fc51e388596e
Anatoly <rook11@list.ru>
2019-08-11 13:26:11 +0300

    master: add links&tooltip animation

---
+++ js/tooltips.js
@@ -0,0 +1,9 @@
+const anchors = document.querySelectorAll(".anchor-tooltip");
+
+anchors.forEach(function (anchor) {
+    const tooltipText = anchor.getAttribute("title"),
+        tooltip = document.createElement("span");
+    tooltip.className = "title-tooltip";
+    tooltip.innerHTML = tooltipText;
+    anchor.appendChild(tooltip);
+});
